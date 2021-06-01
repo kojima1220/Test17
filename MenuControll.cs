c763ac810ba62644f1d34e4913e7739bf2c7a10b
kojima1220 <souheixluux1220@yahoo.co.jp>
2021-06-01 15:43:04 +0900

    試しただけ

---
+++ MenuControll.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuControll : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject MakeRoomButton;
+    public GameObject SelectRoomButton;
+    public GameObject ReturnButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
